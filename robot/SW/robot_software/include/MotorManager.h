91266ba9298b94093084ac1fc037b60d2a6a8663
michael <michalbaz@poczta.onet.pl>
2024-03-21 16:19:51 +0100

    implemented motor manager, timeout TBD

--- robot/SW/robot_software/include/MotorManager.h
+++ robot/SW/robot_software/include/MotorManager.h
@@ -5,9 +5,25 @@
 
 namespace Motor{
 
+constexpr uint32_t motor_num = 2;
+constexpr float inertia_coef = 0.85f;
+
 class MotorManager{
 public:
-    
+    void init() noexcept;
+    void setSpeed(const custom::array<int32_t, motor_num>& speeds) noexcept;
+    MotorManager(float i = inertia_coef) noexcept;
+    void InertiaCoef(float i) noexcept;
+    auto InertiaCoef() const noexcept;
+    void poolMotors() noexcept;
+    const auto& CurrentSpeed() const noexcept;
+    const auto& DesiredSpeed() const noexcept;
+
+private:
+    float m_inertiaCoef;
+    custom::array<IDCMotor*, motor_num> motors;
+    custom::array<int32_t, motor_num> desired_speed;
+    custom::array<int32_t, motor_num> current_speed;
 };
 
 }
---
+++ robot/SW/robot_software/src/MotorManager.cc
@@ -0,0 +1,72 @@
+#include "../MotorManager.h"
+
+namespace Motor{
+
+enum{
+    update_speed = 0,
+    timeout
+};
+
+MotorManager::MotorManager(float i) noexcept:
+    m_inertiaCoef{ i } {}
+
+void MotorManager::init() noexcept{
+    for(auto& m : motors){
+        (*m).init();
+    }
+
+    for(auto& s : desired_speed){
+        s = 0;
+    }
+
+    for(auto& s : current_speed){
+        s = 0;
+    }
+}
+
+void MotorManager::setSpeed(const custom::array<int32_t, motor_num>& speeds) noexcept{
+    for(std::size_t i = 0; i < motor_num; ++i){
+        current_speed[i] = speeds[i];
+    }
+}
+
+void MotorManager::poolMotors() noexcept{
+    static uint8_t state{ update_speed };
+
+    switch(state){
+    case update_speed:
+        auto x = m_inertiaCoef;
+        auto y = 1.0f - m_inertiaCoef;
+
+        for(std::size_t i = 0; i < motor_num; ++i){
+            current_speed[i] = current_speed[i] * x + desired_speed[i] * y;
+            motors[i]->setSpeed(current_speed[i]);
+        }
+
+        state = timeout;
+    break;
+
+    case timeout:
+        // TBD
+        state = update_speed;
+    break;
+    }
+}
+
+void MotorManager::InertiaCoef(float i) noexcept{
+    m_inertiaCoef = i;
+}
+
+auto MotorManager::InertiaCoef() const noexcept{
+    return m_inertiaCoef;
+}
+
+const auto& MotorManager::CurrentSpeed() const noexcept{
+    return current_speed;
+}
+
+const auto& MotorManager::DesiredSpeed() const noexcept{
+    return current_speed;
+}
+
+}
