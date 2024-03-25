27572e1c0e562c5611d66173ee71a41370fd1464
michael <michalbaz@poczta.onet.pl>
2024-03-25 14:08:37 +0100

    implemented IOdometry

--- robot/SW/robot_software/include/interfaces/IOdometry.h
+++ robot/SW/robot_software/include/interfaces/IOdometry.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "../SensorManager.h"
+
+namespace Sensor{
+
+class IOdometry{
+public:
+    virtual ~IOdometry() { }
+    virtual void poolOdometry(SensorManager& s) noexcept = 0;
+    virtual void resetOdometry() noexcept = 0;
+    virtual void init() noexcept;
+};
+
+}
