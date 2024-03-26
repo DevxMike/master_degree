c2530f3c33df79471c5a8f509b5076a5809e9f05
michael <michalbaz@poczta.onet.pl>
2024-03-26 15:01:53 +0100

    finished working on libraries

--- robot/SW/robot_software/include/CommManager.h
+++ robot/SW/robot_software/include/CommManager.h
@@ -1,1 +1,54 @@
 #pragma once
+
+#include "WiFiManager.h"
+#include "stack.h"
+
+namespace Comm{
+namespace MQTT{
+
+constexpr std::size_t stack_depth = 20;
+
+enum class MQTTStatus{
+    NotConnected,
+    Connected
+};
+
+template<class StringType>
+class CommManager{
+public:
+    CommManager(
+        WifiManager&& w, 
+        const StringType& device_id, 
+        const StringType& broker, 
+        StringType password
+    ) noexcept:
+        m_wifiMgr{ std::move(w) }, 
+        m_messageStack{  }, 
+        m_broker{ broker },
+        m_deviceID{ device_id },
+        m_password{ password } {  }
+
+    MQTTStatus poolCommManager() noexcept {
+        // TBD
+        return MQTTStatus::NotConnected;
+    }
+    
+    bool sendMessage(const StringType& message) noexcept {
+        return m_messageStack.push(message);
+    }
+
+private:
+    NetworkStatus poolNetwork() noexcept {
+        // TBD
+        return NetworkStatus::NotConnected;
+    }
+
+    WiFiManager m_wifiMgr; // network management
+    custom::stack<StringType, stack_depth> m_messageStack;
+    StringType m_broker; // MQTT access
+    StringType m_password;
+    StringType m_deviceID;
+};
+
+} // MQTT
+} // Comm
--- robot/SW/robot_software/include/WiFiManager.h
+++ robot/SW/robot_software/include/WiFiManager.h
@@ -1,1 +1,15 @@
 #pragma once
+
+namespace Comm{
+
+enum class NetworkStatus{
+    NotConnected,
+    Connected
+};
+
+template<class HandleType>
+class WiFiManager{
+    // TBD on platform
+};
+
+}
