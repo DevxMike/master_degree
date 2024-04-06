7d753f69ed347ce6c9e82e399f6ffecf9df1958b
michael <michalbaz@poczta.onet.pl>
2024-04-06 22:19:40 +0200

    implemented init and part of DistanceSensor::poolSensor

--- robot/SW/ESP32_project/robot_SW_ESP32/src/DistanceSensor.cc
+++ robot/SW/ESP32_project/robot_SW_ESP32/src/DistanceSensor.cc
@@ -1,16 +1,37 @@
 #include "DistanceSensor.h"
+#include "Arduino.h"
 
 namespace Sensor{
 
+constexpr unsigned long distanceSensorTimeout = 100;
+
 DistanceSensor::DistanceSensor(uint32_t echoPin, uint32_t trigPin) noexcept :
-    m_trigPin{ trigPin }, m_echoPin{ echoPin }, reading{ test_value } { }
+    m_trigPin{ trigPin }, m_echoPin{ echoPin }, reading{ test_value } { State(SensorStates::SensorInit); }
 
 void DistanceSensor::init() noexcept {
-    // TBD
+    pinMode(m_trigPin, OUTPUT);
+    pinMode(m_echoPin, INPUT);
+    State(SensorStates::Timeout);
 }
 
 Sensor::SensorStates DistanceSensor::poolSensor() noexcept {
-    // TBD
+    static unsigned long timer{ 0 };
+
+    switch(State()){
+        case SensorStates::Measuring:
+            // logic TBD
+
+            timer = millis();
+            State(SensorStates::Timeout);
+        break;
+
+        case SensorStates::Timeout:
+            if(millis() - timer > distanceSensorTimeout){
+                State(SensorStates::Measuring);
+            }
+        break;
+    }
+
     return State();
 }
 
