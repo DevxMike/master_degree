ba6bb513ca1303e471127ae603304df5c31670db
michael <michalbaz@poczta.onet.pl>
2024-06-24 22:16:07 +0200

    changed time domain on charts and tables to [ms}

--- thesis/figures/static_algo/charts/nowalls/main.py
+++ thesis/figures/static_algo/charts/nowalls/main.py
@@ -8,13 +8,19 @@ max_times = [0.0964, 0.1067, 0.2229, 0.7815, 3.0381]
 dijkstra_times = [0.0171, 0.0593, 0.1247, 0.3985, 1.5084]
 best_astar_times = [0.0762, 0.0889, 0.2118, 0.7815, 3.0142]
 
+manhattan_times_ms = [time * 1000 for time in manhattan_times]
+euclidean_times_ms = [time * 1000 for time in euclidean_times]
+max_times_ms = [time * 1000 for time in max_times]
+dijkstra_times_ms = [time * 1000 for time in dijkstra_times]
+best_astar_times_ms = [time * 1000 for time in best_astar_times]
+
 # Wykres czasu działania algorytmu A* dla różnych heurystyk
 plt.figure(figsize=(10, 6))
-plt.plot(graph_sizes, manhattan_times, label='Manhattan', marker='o')
-plt.plot(graph_sizes, euclidean_times, label='Euclidean', marker='o')
-plt.plot(graph_sizes, max_times, label='Max', marker='o')
+plt.plot(graph_sizes, manhattan_times_ms, label='Manhattan', marker='o')
+plt.plot(graph_sizes, euclidean_times_ms, label='Euclidean', marker='o')
+plt.plot(graph_sizes, max_times_ms, label='Max', marker='o')
 plt.xlabel('Rozmiary grafu [-]')
-plt.ylabel('Czas [s]')
+plt.ylabel('Czas [ms]')
 plt.title('Czas działania algorytmu A* dla różnych metryk')
 plt.legend()
 plt.grid(True)
@@ -23,9 +29,9 @@ plt.close()
 
 # Wykres czasu działania algorytmu Dijkstry
 plt.figure(figsize=(10, 6))
-plt.plot(graph_sizes, dijkstra_times, label='Dijkstra', color='orange', marker='o')
+plt.plot(graph_sizes, dijkstra_times_ms, label='Dijkstra', color='orange', marker='o')
 plt.xlabel('Rozmiary grafu [-]')
-plt.ylabel('Czas [s]')
+plt.ylabel('Czas [ms]')
 plt.title('Czas działania algorytmu Dijkstry')
 plt.legend()
 plt.grid(True)
@@ -34,10 +40,10 @@ plt.close()
 
 # Wykres porównujący najlepsze wyniki A* z wynikami Dijkstry
 plt.figure(figsize=(10, 6))
-plt.plot(graph_sizes, best_astar_times, label='Najlepszy czas A*', marker='o')
-plt.plot(graph_sizes, dijkstra_times, label='Czas Dijkstry', marker='o')
+plt.plot(graph_sizes, best_astar_times_ms, label='Najlepszy czas A*', marker='o')
+plt.plot(graph_sizes, dijkstra_times_ms, label='Czas Dijkstry', marker='o')
 plt.xlabel('Rozmiary grafu [-]')
-plt.ylabel('Czas [s]')
+plt.ylabel('Czas [ms]')
 plt.title('Porównanie najlepszych wyników A* i Dijkstry')
 plt.legend()
 plt.grid(True)
--- thesis/figures/static_algo/charts/walls/main.py
+++ thesis/figures/static_algo/charts/walls/main.py
@@ -7,6 +7,11 @@ astar_euclidean_times = [0.1030, 0.1216, 0.1309, 0.1048, 0.0923]
 astar_max_times = [0.0991, 0.1197, 0.1425, 0.1020, 0.0984]
 dijkstra_times = [0.0234, 0.0169, 0.0187, 0.0184, 0.0182]
 
+astar_manhattan_times_ms = [time * 1000 for time in astar_manhattan_times]
+astar_euclidean_times_ms = [time * 1000 for time in astar_euclidean_times]
+astar_max_times_ms = [time * 1000 for time in astar_max_times]
+dijkstra_times_ms = [time * 1000 for time in dijkstra_times]
+
 astar_manhattan_lengths = [247, 243, 247, 233, 243]
 astar_euclidean_lengths = [247, 243, 247, 233, 243]
 astar_max_lengths = [247, 243, 247, 233, 243]
@@ -14,12 +19,12 @@ dijkstra_lengths = [247, 243, 247, 233, 243]
 
 # Wykres czasu działania algorytmów
 plt.figure(figsize=(10, 6))
-plt.plot(iterations, astar_manhattan_times, label='A* Manhattan', marker='o')
-plt.plot(iterations, astar_euclidean_times, label='A* Euclidean', marker='o')
-plt.plot(iterations, astar_max_times, label='A* Max', marker='o')
-plt.plot(iterations, dijkstra_times, label='Dijkstra', marker='o')
+plt.plot(iterations, astar_manhattan_times_ms, label='A* Manhattan', marker='o')
+plt.plot(iterations, astar_euclidean_times_ms, label='A* Euclidean', marker='o')
+plt.plot(iterations, astar_max_times_ms, label='A* Max', marker='o')
+plt.plot(iterations, dijkstra_times_ms, label='Dijkstra', marker='o')
 plt.xlabel('Iteracja')
-plt.ylabel('Czas działania algorytmu [s]')
+plt.ylabel('Czas działania algorytmu [ms]')
 plt.title('Czas działania algorytmów w różnych iteracjach')
 plt.legend()
 plt.grid(True)
